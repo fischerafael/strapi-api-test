341bc4700771882b135cfcdc0d545a191bb8dcf3
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-04-07 10:49:36 -0300

    initial commit

---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5e57510c-9b0a-4189-b283-d7a76ddc965d'
+};
---
+++ mongo.md
@@ -0,0 +1,1 @@
+mongodb+srv://strapi-api:strapi-api@cluster0.qqsuz.mongodb.net/strapi-api?retryWrites=true&w=majority
